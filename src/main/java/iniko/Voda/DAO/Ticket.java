ebfdd8eeab31feb2c94bf62837a0b4190236624f
inikolao <alampampi@hotmail.com>
2023-02-21 17:01:44 +0200

    basic theme complete. Exlpore need impovements. Font issue. first dao

--- src/main/java/iniko/Voda/DAO/Ticket.java
+++ src/main/java/iniko/Voda/DAO/Ticket.java
@@ -1,4 +1,28 @@
 package iniko.Voda.DAO;
 
+import jdk.jfr.Relational;
+
+import javax.persistence.*;
+
+@Entity
+@Table( name = "Tickets")
 public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "flight_id")
+    private flight flight;
+
+
+    public iniko.Voda.DAO.flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(iniko.Voda.DAO.flight flight) {
+        this.flight = flight;
+    }
 }
--- src/main/java/iniko/Voda/DAO/User.java
+++ src/main/java/iniko/Voda/DAO/User.java
@@ -1,4 +1,13 @@
 package iniko.Voda.DAO;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table( name = "Users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
 }
